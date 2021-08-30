fa1010054028e4cb5d22e67d14c9be843902c867
Susmit Singh <singhsus25mit@gmail.com>
2021-08-30 10:45:45 +0530

    completed W 43

--- W43/app.js
+++ W43/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const path = require("path");
+const ExpressError = require("./utils/expressError");
+const catchAsync = require("./utils/catchAsync");
 const methodOverride = require("method-override");
 const Campground = require("./models/campgrounds");
+const validationSchema = require("./validationSchema");
+const { campgroundSchema } = require("./lec/lec/schemas");
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp" , {
@@ -14,6 +19,7 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp" , {
 .then(()=>{console.log("MONGO DB IS CONNECTED")})
 .catch(err => {console.log(err)});
 
+app.engine("ejs" , ejsMate);
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views"));
 
@@ -24,47 +30,72 @@ app.get("/" , (req , res) => {
     res.redirect("/campgrounds");
 })
 
-app.get("/campgrounds", async (req, res) => {
+function campgroundFormValidation(req , res , next) {
+    const {campground} = req.body;
+    const {value , error} = campgroundSchema.validate(req.body);
+    if (error) {
+        const message = error.details[0].message;
+        const err = new ExpressError(message , 400);
+        next(err);
+    }
+    else next();
+}
+
+app.get("/campgrounds", catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
-});
+}));
 
-app.post("/campgrounds" , async (req , res) => {
+app.post("/campgrounds" , campgroundFormValidation , catchAsync(async (req , res) => {
     const { campground } = req.body;
     const newSite = new Campground(campground);
     await newSite.save();
     res.redirect("/campgrounds");
-});
+}));
 
 app.get("/campgrounds/new" , (req , res) => {
     res.render("campgrounds/new");
 })
 
-app.get("/campgrounds/:id" , async (req , res) => {
+app.get("/campgrounds/:id" , catchAsync(async (req , res) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     res.render("campgrounds/show" , {campground});
-})
+}));
 
-app.get("/campgrounds/:id/update" , async (req , res) => {
+app.get("/campgrounds/:id/update" , catchAsync(async (req , res) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     res.render("campgrounds/update" , {campground});
-})
+}));
 
-app.put("/campgrounds/:id" , async (req , res) => {
+app.put("/campgrounds/:id" , campgroundFormValidation , catchAsync(async (req , res) => {
     const {id} = req.params;
     const campground = await Campground.findById(id);
     campground.title = req.body.campground.title;
     campground.location = req.body.campground.location;
+    campground.price = req.body.campground.price;
+    campground.description = req.body.campground.description;
+    campground.image = req.body.campground.image;
     await campground.save();
     res.redirect(`/campgrounds/${id}`);
-})
+}));
 
-app.delete("/campgrounds/:id" , async(req , res) => {
+app.delete("/campgrounds/:id" , catchAsync(async(req , res) => {
     const {id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+}));
+
+app.use((req , res , next) => {
+    console.log("this was hit");
+    throw new ExpressError("No path was found :(" , 404);
+})
+
+app.use((err , req , res , next) => {
+    if (!err.status) err.status = 500;
+    if (!err.message) err.message = "Something Went Wrong";
+    res.status(err.status).render("error" , {err});
 })
 
 app.listen(PORT , () => {
--- W43/models/campgrounds.js
+++ W43/models/campgrounds.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
 
 const campgroundSchema = new mongoose.Schema({
-    title: String,
-    price: String,
-    description: String,
-    location: String
+    title: {
+        type : String,
+        required : true
+    },
+    price: {
+        type : Number,
+        required : true,
+        min : [0 , "The Minimum Price should be 0"]
+    },
+    description: {
+        type : String,
+        required : true
+    },
+    location: {
+        type : String,
+        required : true
+    },
+    image : {
+        type : String,
+        required : true
+    },
 });
 
 module.exports = mongoose.model("Campground" , campgroundSchema);
